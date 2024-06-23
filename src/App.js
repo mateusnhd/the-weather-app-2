618d47017ded2e0f0956570689f69ab47410ea92
João Luft <joaoluft26@gmail.com>
2024-06-23 13:47:57 -0300

    Feat: forecast

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import { GlobalStyles } from "themes/globalStyles";
 import { useWeather } from "hooks/useWeather";
 
 export const App = () => {
-  const { searchLocation, data, search, setSearch } = useWeather();
+  const { searchLocation, weather, forecast, setForecast, search, setSearch } = useWeather();
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Container>
-        { !data && (
+        { !weather && (
           <FindContainer>
             <Title>Weather App</Title>
             <SearchInput fn={searchLocation} setValue={setSearch} value={search} />
           </FindContainer>
         )}
-        { data && <Weather data={data} />}
+        { weather && <Weather weatherData={weather} forecast={forecast} setForecast={setForecast} />}
       </Container>
     </ThemeProvider>
   );
